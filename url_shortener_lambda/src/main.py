965ede1493cdced06ee490d4b1239f8071fe3696
erichrobinson <erich.robinson@guildeducation.com>
2020-11-02 09:19:35 -0700

    add link to table

--- url_shortener_lambda/src/main.py
+++ url_shortener_lambda/src/main.py
@@ -1,2 +1,10 @@
-def handler():
-    return True
+import os
+
+import boto3
+
+from url_shortener_handler import UrlShortenerHandler
+
+dynamodb = boto3.resource("dynamodb", region_name="us-west-2")
+link_table = dynamodb.Table(os.environ["LINK_TABLE_NAME"])
+
+handler = UrlShortenerHandler(link_table=link_table)
